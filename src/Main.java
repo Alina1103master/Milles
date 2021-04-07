20d4bdd750197aa05192544e46b0edab2549cbe8
Alina <77508351+Alina1103master@users.noreply.github.com>
2021-04-07 12:16:26 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+    int price = 23540;
+    int part = 20;
+    int milles = price / part * 1;
+    System.out.println(milles);
+    }
+}
